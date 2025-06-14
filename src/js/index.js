bb82b3451442517942fb3f5323dbf94422cd1a7f
Renzo Luque <80488682+renzoluquem@users.noreply.github.com>
2025-06-14 13:15:17 -0500

    feat: add initial project setup with homepage sections, styles, and JavaScript functionality

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('open-login-modal');
+    const registerButton = document.getElementById('open-register-modal');
+});
