1ec3ecccf3d54d3cd67b22b70c39dc32a6d6cebf
Lehonel <u20231b801@upc.edu.pe>
2025-06-16 20:28:18 -0500

    Actualización de archivos

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('open-login-modal');
     const registerButton = document.getElementById('open-register-modal');
+
+    loginButton.addEventListener('click', () => {
+        window.location.href = "/src/Login.html"
+    })
+
+    registerButton.addEventListener('click', () => {
+        window.location.href =  "/src/Registrer.html"
+    })
 });
+
+
+
+
+
+
+
+function Login(){
+    window.location.href = "/src/Login.html"
+}
+
+function Registrar(){
+    window.location.href = "/src/Registrer.html"
+}
+
+
