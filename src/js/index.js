85ebaf526ea7f946c5e48c5b92102fe82349631b
Lehonel <u20231b801@upc.edu.pe>
2025-07-10 11:00:09 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,20 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const registerButton = document.getElementById('open-register-modal');
 
     loginButton.addEventListener('click', () => {
-        window.location.href = "../src/login.html"; // Correct relative path
+        window.location.href = "src/login.html"; // Correct relative path
     });
 
     registerButton.addEventListener('click', () => {
-        window.location.href = "../src/register.html"; // Correct relative path
+        window.location.href = "src/register.html"; // Correct relative path
     });
 });
 
 function Login() {
-    window.location.href = "../src/login.html"; // Correct relative path
+    window.location.href = "src/login.html"; // Correct relative path
 }
 
 function Registrar() {
-    window.location.href = "../src/register.html"; // Correct relative path
+    window.location.href = "src/register.html"; // Correct relative path
 }
 
 
