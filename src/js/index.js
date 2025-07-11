adc0df26437d9b667984153ac2048c8c163ddb23
Lehonel <u20231b801@upc.edu.pe>
2025-07-11 11:05:26 -0500

    Guardando mis cambios locales antes del merge

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('open-login-modal');
     const registerButton = document.getElementById('open-register-modal');
+    const loginFromRegister = document.getElementById('login-from-register');
+    const registerFromLogin = document.getElementById('register-from-login');
+    const botonChatBot = document.getElementById('btn-live-chat')
+    const Volver = document.getElementById('btn-back');
 
-    loginButton.addEventListener('click', () => {
-        window.location.href = "../src/login.html"; // Correct relative path
-    });
+    if (loginButton) {
+        loginButton.addEventListener('click', () => {
+            window.location.href = "../src/Login.html";
+        });
+    }
 
-    registerButton.addEventListener('click', () => {
-        window.location.href = "../src/register.html"; // Correct relative path
-    });
+    if (registerButton) {
+        registerButton.addEventListener('click', () => {
+            window.location.href = "../src/register.html";
+        });
+    }
+
+    if (loginFromRegister) {
+        loginFromRegister.addEventListener('click', () => {
+            window.location.href = "../src/Login.html";
+        });
+    }
+
+    if (registerFromLogin) {
+        registerFromLogin.addEventListener('click', () => {
+            window.location.href = "../src/register.html";
+        });
+    }
+
+    if(botonChatBot){
+        botonChatBot.addEventListener('click',()=> {
+            window.location.href = "../src/chat-bot.html";
+        });
+    }
+
+    if (Volver) {
+        Volver.addEventListener('click', () => {
+            window.history.back();
+        });
+    }
 });
 
-function Login() {
-    window.location.href = "../src/login.html"; // Correct relative path
-}
 
-function Registrar() {
-    window.location.href = "../src/register.html"; // Correct relative path
-}
+
 
 
